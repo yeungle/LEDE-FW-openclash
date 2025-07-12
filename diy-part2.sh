5bc868259ab84c18ee61694ec875d16f2b0a893f
yeungle <75871729+yeungle@users.noreply.github.com>
2025-07-12 11:30:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,10 +21,8 @@
 
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-adguardhome
-rm -rf feeds/luci/applications/luci-app-smartdns
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/adguardhome
-rm -rf feeds/packages/net/smartdns
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/utils/v2dat
 
@@ -39,11 +37,6 @@ cp -rf kenzok8-packages/wrtbwmon package/wrtbwmon
 cp -rf kenzok8-packages/luci-app-wrtbwmon package/luci-app-wrtbwmon
 cp -rf kenzok8-packages/adguardhome package/adguardhome
 cp -rf kenzok8-packages/luci-app-adguardhome package/luci-app-adguardhome
-cp -rf kenzok8-packages/smartdns package/smartdns
-cp -rf kenzok8-packages/luci-app-smartdns package/luci-app-smartdns
-#cp -rf kenzok8-packages/mosdns package/mosdns
-#cp -rf kenzok8-packages/luci-app-mosdns package/luci-app-mosdns
-#cp -rf kenzok8-packages/v2dat package/v2dat
 rm -rf kenzok8-packages
 
 git clone --depth=1 -b v5 https://github.com/sbwml/luci-app-mosdns.git mosdns-packages
@@ -54,8 +47,6 @@ rm -rf mosdns-packages
 
 #git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-
-sed -i '/commit firewall/a\	set uhttpd.main.max_requests=50\n	commit uhttpd' feeds/helloworld/luci-app-ssr-plus/root/etc/uci-defaults/luci-ssr-plus
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
