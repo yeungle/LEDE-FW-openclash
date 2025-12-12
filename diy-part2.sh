bfdb982b59066f4e2fd8e679eebdf6bc2fdac16a
yeungle <75871729+yeungle@users.noreply.github.com>
2025-12-12 14:15:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,7 +24,7 @@ rm -rf feeds/luci/applications/luci-app-adguardhome
 rm -rf feeds/packages/net/adguardhome
 rm -rf feeds/luci/applications/luci-app-openclash
 
-git clone --depth=1 https://github.com/kenzok8/small-package.git kenzok8-packages
+git clone https://github.com/kenzok8/small-package.git kenzok8-packages
 cp -rf kenzok8-packages/ddnsto package/ddnsto
 cp -rf kenzok8-packages/luci-app-ddnsto package/luci-app-ddnsto
 cp -rf kenzok8-packages/wrtbwmon package/wrtbwmon
@@ -33,11 +33,11 @@ cp -rf kenzok8-packages/adguardhome package/adguardhome
 cp -rf kenzok8-packages/luci-app-adguardhome package/luci-app-adguardhome
 rm -rf kenzok8-packages
 
-git clone --depth=1 https://github.com/vernesong/OpenClash.git OpenClash-packages
+git clone https://github.com/vernesong/OpenClash.git OpenClash-packages
 cp -rf OpenClash-packages/luci-app-openclash package/luci-app-openclash
 rm -rf OpenClash-packages
 
-git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
 #./scripts/feeds update -a
 #./scripts/feeds install -a
