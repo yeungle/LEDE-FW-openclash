eb64b82eb031c2ad8d1cc621981878c825b5d1f2
yeungle <75871729+yeungle@users.noreply.github.com>
2025-12-18 10:49:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -33,9 +33,9 @@ cp -rf kenzok8-packages/adguardhome package/adguardhome
 cp -rf kenzok8-packages/luci-app-adguardhome package/luci-app-adguardhome
 rm -rf kenzok8-packages
 
-git clone --depth=1 https://github.com/vernesong/OpenClash.git OpenClash-packages
-cp -rf OpenClash-packages/luci-app-openclash package/luci-app-openclash
-rm -rf OpenClash-packages
+git clone --depth=1 https://github.com/vernesong/OpenClash.git package/OpenClash
+
+git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter 
 
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
